768635819099306c8e55ccdb73aab7f20cb419de
Waltertique <johanmadrigal66@gmail.com>
2025-08-13 20:22:13 -0500

    conexion

--- EduMultiPro 4 Parte/Frontend y Backend de EduMultiPro/Frontend/edumultipro/src/Desplegable.jsx
+++ EduMultiPro 4 Parte/Frontend y Backend de EduMultiPro/Frontend/edumultipro/src/Desplegable.jsx
@@ -13,13 +13,13 @@ function Desplegable() {
                         Opciones
                         </button>
                         <ul className="dropdown-menu">
-                        <li><Link to="/usuario"><i className="fa-solid fa-user"><p>Usuario</p></i></Link></li>
-                        <li><Link to="/curso"><i className="fa-solid fa-layer-group"><p>Cursos</p></i></Link></li>
-                        <li><Link to="/horario"><i className="fa-solid fa-calendar-days"><p>Horarios</p></i></Link></li>
-                        <li><Link to="/aula"><i className="fa-solid fa-user-group"><p>Aulas</p></i></Link></li>
-                        <li><Link to="/noticia"><i className="fa-solid fa-newspaper"><p>Noticias</p></i></Link></li>
-                        <li><Link to="/Reportes"><i class="fa-regular fa-folder-open"><p>Reportes</p></i></Link></li>
-                        <li><Link to="/"><i className="fa-solid fa-right-to-bracket"><p>Salir</p></i></Link></li>
+                            <li><Link to="/usuario"><i className="fa-solid fa-user"><p>Usuario</p></i></Link></li>
+                            <li><Link to="/curso"><i className="fa-solid fa-layer-group"><p>Cursos</p></i></Link></li>
+                            <li><Link to="/horario"><i className="fa-solid fa-calendar-days"><p>Horarios</p></i></Link></li>
+                            <li><Link to="/aula"><i className="fa-solid fa-user-group"><p>Aulas</p></i></Link></li>
+                            <li><Link to="/noticia"><i className="fa-solid fa-newspaper"><p>Noticias</p></i></Link></li>
+                            <li><Link to="/Reportes"><i class="fa-regular fa-folder-open"><p>Reportes</p></i></Link></li>
+                            <li><Link to="/"><i className="fa-solid fa-right-to-bracket"><p>Salir</p></i></Link></li>
                         </ul>
                     </div>
                 </div>
